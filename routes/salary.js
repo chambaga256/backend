40e199996beaad585636a84876c99752c060fa2e
chambaga256 <chambagaabudlah@gmail.com>
2024-01-05 15:08:10 +0300

    uodatsest

--- routes/salary.js
+++ routes/salary.js
@@ -52,4 +52,45 @@ router.post("/salary", async (req, res) => {
   }
 });
 
+
+
+// update an existing transaction/expense
+router.put("/salary/:id", async (req, res) => {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  // validate incoming data
+
+  console.log(req.body)
+  const { error } = validateSalary(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const decodedToken = decodeToken(token);
+  console.log(decodeToken._id)
+
+  try {
+    // Find the salary record by ID and creator to ensure the user can update their own record
+    const salary = await Salary.findOne({ _id: req.params.id, createdBy: decodedToken._id });
+console.log("salary...",salary)
+    if (!salary) {
+      return res.status(404).send("Salary record not found");
+    }
+
+    // Update the salary record with the new amount
+    salary.amount = req.body.amount;
+
+    // Save the updated record
+    const updatedSalary = await salary.save();
+
+    // Send the updated salary record as a response
+    res.status(200).send(updatedSalary);
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
 module.exports = router;
