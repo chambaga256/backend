8f9c90c09bf3faa75c4cd5a299cdff44ac258f73
chambaga256 <chambagaabudlah@gmail.com>
2024-10-25 11:28:39 +0300

    updates

--- routes/category.js
+++ routes/category.js
@@ -15,7 +15,7 @@ router.post('/categories', async (req, res) => {
 
   router.get('/categories', async (req, res) => {
     try {
-      const categories = await Category.find();
+      const categories = await category.find();
       res.status(200).json(categories);
     } catch (error) {
       res.status(500).json({ error: 'Failed to fetch categories' });
